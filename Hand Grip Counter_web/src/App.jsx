1c33ec171f4355a00bd26c62700877d5196c060b
Nicola Alivant <nicola.alivant@gmail.com>
2025-04-28 15:48:25 +0700

    hide bottom navigation when routing to add

--- Hand Grip Counter_web/src/App.jsx
+++ Hand Grip Counter_web/src/App.jsx
@@ -1,31 +1,45 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  useLocation,
+} from "react-router-dom";
 import Add from "./components/add/Add";
 import List from "./components/list/List";
 import Header from "./Header";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Chart from "./components/chart/Chart";
+import CounterChart from "./components/chart/CounterChart";
 import BottomNavBar from "./BottomNavBar";
 import "./app.css";
 
-function App() {
+function MainContent() {
+  const location = useLocation();
+  const hideBottomNavRoutes = ["/add"];
+
+  const shouldHideBottomNav = hideBottomNavRoutes.includes(location.pathname);
+
   return (
     <div className="App">
-      <Router>
-        <Header />
-        <div className="content m-3">
-          <Routes>
-            <Route exact path="/" Component={List}></Route>
-            <Route exact path="/chart" Component={Chart}></Route>
-            <Route path="/add" Component={Add}></Route>
-          </Routes>
-          <ToastContainer position="top-right" autoClose={2000} />
-        </div>
-        <BottomNavBar />
-      </Router>
+      <Header />
+      <div className="content m-3">
+        <Routes>
+          <Route exact path="/" Component={List}></Route>
+          <Route exact path="/counter_chart" Component={CounterChart}></Route>
+          <Route path="/add" Component={Add}></Route>
+        </Routes>
+        <ToastContainer position="top-right" autoClose={2000} />
+      </div>
+      {!shouldHideBottomNav && <BottomNavBar />}
     </div>
   );
 }
 
-export default App;
+export default function App() {
+  return (
+    <Router>
+      <MainContent />
+    </Router>
+  );
+}
